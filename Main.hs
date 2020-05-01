ac000dda17bed91c89dbb448ed8c6350c5b8cf41
Kei Sugano <kei.sugano@h-t.co.jp>
2020-05-01 12:48:08 +0900

    bunch of files added, the command line parsing part is on the way to be implemented

--- Main.hs
+++ Main.hs
@@ -1,5 +1,21 @@
+-- create Groovy analyzer and kinda lint tool 
+
+import Data.List
+import Data.List.Split
 import System.Environment
 
+-- TODO: need to parse the command line argument.
+--       if command line argument as the directory path missing,
+--       it is going to refer to the current directory 
+
 main :: IO ()
-main = getArgs >>= print . haqify . head
-haqify s = "Haq " ++ s
+main = do
+    -- First, need to extract the action param, which starts from '-'
+    -- -l        ... wordCount analyzer 
+    -- --groovy  ... groocy syntax analyzer
+    args <- getArgs 
+    let actions    = ["-l", "--groovy"]
+    let parsedArgs = [lines x | x <- splitOn "-" $ unlines args, x /= ""]
+    print parsedArgs
+    return ()
+
